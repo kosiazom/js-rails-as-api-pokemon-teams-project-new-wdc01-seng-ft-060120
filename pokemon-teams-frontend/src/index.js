17ef11d39ada65f3a71a3d1fe2da40e5e9cba463
Kosi Akporji <kosiazom@gmail.com>
2020-07-20 15:39:54 -0400

    rendered teams, created controllers and backend item, associated trainers and pokemons

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,38 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+fetch(TRAINERS_URL)
+.then(resp => resp.json())
+.then( trainersObject => {renderAllTeams(trainersObject)})
+
+
+function renderAllTeams(trainersObject) {
+    trainersObject.forEach(trainer => {renderATeam(trainer)
+         });
+}
+
+function renderATeam(trainer) {
+    let div = document.createElement('div')
+    div.className = 'card'
+    div.dataset.number = trainer.id
+
+    let button = document.createElement('button')
+    button.dataset.number = trainer.id
+    
+    let ul = document.createElement('ul')
+    
+    let li = document.createElement('li')
+
+    let classButton = document.createElement('button')
+    classButton.className = 'release'
+    classButton
+
+    li.insertAdjacentElement("afterbegin", classButton)
+
+
+
+
+
+}
